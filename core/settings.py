0df2017f0bc5387ae3fe9aef4cf534aa638c19a0
Premkumar <premkumarnunna.ce@gmail.com>
2024-05-09 14:55:04 +0530

    updated the permission settings in settings.py file

--- core/settings.py
+++ core/settings.py
@@ -131,11 +131,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
-    # "DEFAULT_PERMISSION_CLASSES": [
-    #     "rest_framework.permissions.IsAuthenticated",
-    # ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
-
-# 6163090b0feecc8ad49e36464b05c78022cdb2e5
